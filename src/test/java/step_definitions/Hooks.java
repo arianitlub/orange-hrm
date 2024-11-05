1d39d6a3ea8082d14473b5180bb80d0840587b50
Arianit <arianit.lu@hotmail.com>
2024-11-05 20:42:11 +0100

    modify hook to save screenshot locally

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -4,10 +4,13 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import utilities.Driver;
 
+import java.io.File;
+import java.io.IOException;
 
 public class Hooks {
 
@@ -18,10 +21,18 @@ public class Hooks {
 
     @After
     public void takeScreenshot(Scenario scenario){
+        if (scenario.isFailed()) {
+            File screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.FILE);
 
-        if(scenario.isFailed()){
-            byte[] image = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(image, "image/png", scenario.getName());
+            String filePath = "screenshots/" + scenario.getName() + ".png";
+            try {
+                FileUtils.copyFile(screenshot, new File(filePath));
+
+                byte[] image = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(image, "image/png", scenario.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
